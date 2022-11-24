eb9b1e133f96b5aa9b23ef21464040de429f6f92
Konstantin <kot199217@mail.ru>
2022-11-24 12:22:57 +0900

    end Task3

--- HomeWork1.py
+++ HomeWork1.py
@@ -13,4 +13,57 @@
 # else:
 #     print('Нет')
 
+# Напишите программу для. проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
+# for x in (range(0, 2)):
+#     for y in (range(0, 2)):
+#         for z in (range(0, 2)):
+#             print(not(x or y or z) == not x and not y and not z)
+
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и
+# выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+#
+# Пример:
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
+
+# print('Введите ваши координаты через запятую:')
+# some_str = input().split(', ')
+# x = int(some_str[0])
+# y = int(some_str[1])
+# if x > 0 and y > 0:
+#     print('1')
+# if x < 0 and y > 0:
+#     print('2')
+# if x < 0 and y < 0:
+#     print('3')
+# if x > 0 and y < 0:
+#     print('4')
+
+# Напишите программу, которая по заданному номеру четверти,
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
+# print('Введите четверть и узнаем диапазон возможных координат:')
+# num = int(input())
+# if num ==1:
+#     print('x   y')
+#     for i in range(0, 6):
+#         for j in range(0, 6):
+#             print(i, + j, sep=', ')
+# if num ==2:
+#     print('x   y')
+#     for i in range(0, 6):
+#         for j in range(0, 6):
+#             print(i*-1, + j, sep=', ')
+# if num ==3:
+#     print('x   y')
+#     for i in range(0, 6):
+#         for j in range(0, 6):
+#             print(i*-1, + j*-1, sep=', ')
+# if num ==4:
+#     print('x   y')
+#     for i in range(0, 6):
+#         for j in range(0, 6):
+#             print(i, + j*-1, sep=', ')
