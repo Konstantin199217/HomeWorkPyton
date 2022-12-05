92fa828ccbf1795e6b240d2dd134e2856c49ab02
Konstantin <kot199217@mail.ru>
2022-12-05 18:10:53 +0900

    HomeWork4 dop2

--- HomeWork4.py
+++ HomeWork4.py
@@ -50,6 +50,7 @@
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
 import sympy
+from sympy import solve
 import random
 
 # a = int(input('Введите степнь: '))
@@ -69,29 +70,29 @@ import random
 
 # Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
-# a = 2
-# x = sympy.Symbol('x')
-# l = [random.randint(0, 100) for _ in range(7)]
-# f = int(random.randint(1, 4))
-# y = 0
-# if f == 1:
-#     y = l[0] * x ** a
-# if f == 2:
-#     y = l[0] * x ** a + l[1] * x
-# if f == 3:
-#     y = l[0] * x ** a + l[1] * x - l[2]
-# with open('File1.txt', 'w', encoding='utf-8') as file:
-#     file.write(f'{y}' + '\n')
-# r = int(random.randint(1, 4))
-# z = 0
-# if r == 1:
-#     z = l[3] * x ** a
-# if r == 2:
-#     z = l[3] * x ** a + l[4] * x
-# if r == 3:
-#     z = l[3] * x ** a + l[4] * x - l[5]
-# with open('File2.txt', 'w', encoding='utf-8') as file:
-#     file.write(f'{z}'  + '\n')
+a = 2
+x = sympy.Symbol('x')
+l = [random.randint(0, 100) for _ in range(7)]
+f = int(random.randint(1, 4))
+g = 0
+if f == 1:
+    g = l[0] * x ** a
+if f == 2:
+    g = l[0] * x ** a + l[1] * x
+if f == 3:
+    g = l[0] * x ** a + l[1] * x - l[2]
+with open('File1.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{g}' + '\n')
+r = int(random.randint(1, 4))
+z = 0
+if r == 1:
+    z = l[3] * x ** a
+if r == 2:
+    z = l[3] * x ** a + l[4] * x
+if r == 3:
+    z = l[3] * x ** a + l[4] * x - l[5]
+with open('File2.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{z}' + '\n')
 
 with open('File1.txt', 'r', encoding='utf-8') as file:
     file.seek(0, 0)
@@ -102,7 +103,8 @@ with open('File2.txt', 'r', encoding='utf-8') as file:
     y2 = file.read().splitlines()
 
 print(y, y2)
-# ot = sympy.solve(y + y2)
-# with open('File2.txt', 'w', encoding='utf-8') as file:
-#     file.seek(0, 0)
-#     file.write(f'{ot}')
+ot = sympy.solve(y + y2)
+with open('File.txt', 'w', encoding='utf-8') as file:
+    file.seek(0, 0)
+    file.write(f'{ot}')
+print(ot)
