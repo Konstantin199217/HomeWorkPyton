3c671445d38b5f2f6ba7f18f18dd1633da194afa
Konstantin <kot199217@mail.ru>
2022-12-12 16:08:51 +0900

    orbits

--- HomeWork6.py
+++ HomeWork6.py
@@ -7,27 +7,39 @@
 # [1,4,5,2,3,9,8,11,0] => "0-5,8-9,11"
 # [1,4,3,2] => "1-4"
 # [1,4] => "1,4"
+import math
 
-some_list = [1,4,5,2,3,9,8,11,0]
-some_list.sort()
-print((some_list))
-def chang(list):
-    some_str = []
-    mini = list[0]
-    count = list[0]
-    for i in range(len(list)):
-        if list[i] - count == 0:
-            count += 1
-            if list[i] == max(list):
-                some_str.append(str(f'{mini} - {list[i]}'))
-                break
-        else:
-            some_str.append(str(f'{mini}-{count - 1}'))
-            mini = list[i]
-            count = list[i] + 1
-        if list[i] == max(list):
-                some_str.append(str(f'{list[i]}'))
-    return ','.join(some_str)
-print(chang(some_list))
+# some_list = [1,4,5,2,3,9,8,11,0]
+# some_list.sort()
+# print((some_list))
+# def chang(list):
+#     some_str = []
+#     mini = list[0]
+#     count = list[0]
+#     for i in range(len(list)):
+#         if list[i] - count == 0:
+#             count += 1
+#             if list[i] == max(list):
+#                 some_str.append(str(f'{mini} - {list[i]}'))
+#                 break
+#         else:
+#             some_str.append(str(f'{mini}-{count - 1}'))
+#             mini = list[i]
+#             count = list[i] + 1
+#         if list[i] == max(list):
+#                 some_str.append(str(f'{list[i]}'))
+#     return ','.join(some_str)
+# print(chang(some_list))
 
+# Планеты и орбиты
+#S = p*a*b
+#Пример:
+#orbits = [(1,3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+#print(*find_farthest_orbit(orbits))
+# 2.5, 10
+orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+def find_farthest_orbit(orbits):
+    s = [3.14*max(x) for x in orbits]
+    return(orbits[s.index(max(s))])
 
+print(find_farthest_orbit(orbits))
